feb980921d2de04f8f877c060876c29142897bf5
mykidong@gmail.com <mykidong@gmail.com>
2015-06-04 15:58:05 +0900

    read/writeObject to avoid incompatible local class...

--- src/main/java/cascading/scheme/ParquetAvroScheme.java
+++ src/main/java/cascading/scheme/ParquetAvroScheme.java
@@ -25,34 +25,29 @@ import cascading.tap.Tap;
 import cascading.tuple.Tuple;
 
 public class ParquetAvroScheme extends DeprecatedAvroScheme {
-	
-	private static final long serialVersionUID = 1L;
-	
-	
-	public ParquetAvroScheme(Schema schema)
-	{
+
+	public ParquetAvroScheme(Schema schema) {
 		super(schema);
 	}
-	
-	 @Override
+
+	@Override
 	public boolean source(FlowProcess<? extends Configuration> flowProcess,
 			SourceCall<Object[], RecordReader> sourceCall) throws IOException {
 
 		RecordReader<Void, GenericRecord> input = sourceCall.getInput();
-		
+
 		GenericRecord genericRecord = input.createValue();
-		
+
 		if (!input.next(input.createKey(), genericRecord)) {
 			return false;
-		}		
-		
+		}
+
 		Record record = new Record(genericRecord.getSchema());
-		
-		for(Field field : genericRecord.getSchema().getFields())
-		{
+
+		for (Field field : genericRecord.getSchema().getFields()) {
 			record.put(field.name(), genericRecord.get(field.name()));
-		}		
-		
+		}
+
 		Tuple tuple = sourceCall.getIncomingEntry().getTuple();
 		tuple.clear();
 
@@ -61,42 +56,52 @@ public class ParquetAvroScheme extends DeprecatedAvroScheme {
 
 		return true;
 	}
-	
+
+	private void writeObject(java.io.ObjectOutputStream out) throws IOException {
+		out.writeObject(this.schema.toString());
+	}
+
+	private void readObject(java.io.ObjectInputStream in) throws IOException {
+		this.schema = readSchema(in);
+	}
+
 	@Override
-	public void sourceConfInit(FlowProcess<? extends Configuration> flowProcess,
-			Tap<Configuration, RecordReader, OutputCollector> tap, Configuration conf) {
-		
+	public void sourceConfInit(
+			FlowProcess<? extends Configuration> flowProcess,
+			Tap<Configuration, RecordReader, OutputCollector> tap,
+			Configuration conf) {
+
 		retrieveSourceFields(flowProcess, tap);
-	
+
 		// Set the input schema and input class
-		conf.set(AvroReadSupport.AVRO_REQUESTED_PROJECTION, schema.toString());			
-		conf.setClass("mapred.input.format.class", AvroParquetInputFormat.class, InputFormat.class);
-		
-	
+		conf.set(AvroReadSupport.AVRO_REQUESTED_PROJECTION, schema.toString());
+		conf.setClass("mapred.input.format.class",
+				AvroParquetInputFormat.class, InputFormat.class);
+
 		// add AvroSerialization to io.serializations
-		addAvroSerializations(conf);			
+		addAvroSerializations(conf);
 	}
 
 	@Override
 	public void sinkConfInit(FlowProcess<? extends Configuration> flowProcess,
-			Tap<Configuration, RecordReader, OutputCollector> tap, Configuration conf) {
+			Tap<Configuration, RecordReader, OutputCollector> tap,
+			Configuration conf) {
 
 		if (schema == null) {
 			throw new RuntimeException("Must provide sink schema");
 		}
-		
-		
+
 		// Set the input schema and input class
-		conf.set("parquet.avro.schema", schema.toString());		
-		conf.setClass("mapred.output.committer.class", AvroParquetOutputCommitter.class, OutputCommitter.class);	
-		conf.setClass("mapred.output.format.class", AvroParquetOutputFormat.class, OutputFormat.class);
-		
-		
+		conf.set("parquet.avro.schema", schema.toString());
+		conf.setClass("mapred.output.committer.class",
+				AvroParquetOutputCommitter.class, OutputCommitter.class);
+		conf.setClass("mapred.output.format.class",
+				AvroParquetOutputFormat.class, OutputFormat.class);
+
 		// add AvroSerialization to io.serializations
 		addAvroSerializations(conf);
-		
+
 	}
-	
 
 	private void addAvroSerializations(Configuration conf) {
 		Collection<String> serializations = conf
@@ -109,33 +114,33 @@ public class ParquetAvroScheme extends DeprecatedAvroScheme {
 		conf.setStrings("io.serializations",
 				serializations.toArray(new String[serializations.size()]));
 	}
-	
-	
+
 	@Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		if (!super.equals(o))
+			return false;
 
-        ParquetAvroScheme that = (ParquetAvroScheme) o;
+		ParquetAvroScheme that = (ParquetAvroScheme) o;
 
-        if (schema != null ? !schema.equals(that.schema) : that.schema != null) return false;
+		if (schema != null ? !schema.equals(that.schema) : that.schema != null)
+			return false;
 
-        return true;
-    }
+		return true;
+	}
 
-    @Override
-    public String toString() {
-        return "ParquetAvroScheme{" +
-                "schema=" + schema +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "ParquetAvroScheme{" + "schema=" + schema + "}";
+	}
 
-    @Override
-    public int hashCode() {
-    	
-        return 31 * getSinkFields().hashCode() +
-                (schema == null ? 0 : schema.hashCode());
-    }
+	@Override
+	public int hashCode() {
+		return 31 * getSinkFields().hashCode()
+				+ (schema == null ? 0 : schema.hashCode());
+	}
 
 }
